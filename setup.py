7add0ab4964744dea089a6a9b999955177ea383a
shivaram-abhishek <47098858+shivaram-abhishek@users.noreply.github.com>
2020-02-16 21:54:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,45 +1,44 @@
-
 from distutils.core import setup
 
 setup(
 
-  name = 'topsis101703028',         # How you named your package folder (MyLib)
+  name = 'topsis101703028',
 
-  packages = ['topsis101703028'],   # Chose the same as "name"
+  packages = ['topsis101703028'],
 
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',
 
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  license='MIT',
 
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  description = 'Python package to implement TOPSIS',
 
-  author = 'YOUR NAME',                   # Type in your name
+  author = 'Abhishek Shivaram',
 
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
+  author_email = 'ashivaram_be17@thapar.com',
 
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  url = 'https://github.com/shivaram-abhishek/topsis101703028',
 
-  download_url = 'https://github.com/shivaram-abhishek/topsis/archive/0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
 
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  keywords = ['TOPSIS'],
 
-  install_requires=[            # I get to this in a second
+  install_requires=[
 
           'numpy'
-
+    
       ],
 
   classifiers=[
 
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 3 - Alpha',
 
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Intended Audience :: Developers',
 
     'Topic :: Software Development :: Build Tools',
 
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'License :: OSI Approved :: MIT License',
 
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3',
 
     'Programming Language :: Python :: 3.4',
 
