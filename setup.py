dfcc44719691519bf3062f250f9c3afcd1569bc5
shivaram-abhishek <47098858+shivaram-abhishek@users.noreply.github.com>
2020-02-16 21:16:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 
   packages = ['topsis101703028'],   # Chose the same as "name"
 
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
 
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
 
